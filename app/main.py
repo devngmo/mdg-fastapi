92678cb3627127a20d13f09d7664231412264be2
Tran Manh Linh <devngmo@gmail.com>
2023-05-12 07:12:12 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+
 from datetime import datetime, timedelta
 from typing import Union
 
 from fastapi import FastAPI, Body, Header
 from fastapi.middleware.cors import CORSMiddleware
+import random, time
 
 app = FastAPI()
 app.add_middleware(
@@ -28,6 +30,18 @@ def generate_object_info_by_category(category):
     if category == 'book':
         return { 'id': 'book01', 'title': 'Book 1', 'author_id': 'peter_pan', 'author_name': 'Peter Pan'}
     return {"id": 'sample-object-01', "name": 'This is object Name' }
+
+@app.get("/objects/{category}/{pageSize}")
+def generate_objects_info_by_category(category, pageSize:int, delaySeconds: int = Header(default=0)):
+    ls = []
+    for i in range(pageSize):
+        rnd = random.randint(1, 100)
+        ls += [{ 'id': f'{category}-{i}', 'title': f'{category} {i} Item {rnd}', 'author_id': f'author-{rnd}', 'author_name': f'Author {rnd}'}]
+
+    if delaySeconds > 0:
+        print(f'Sleep for {delaySeconds} seconds...')
+        time.sleep(delaySeconds)
+    return ls
 
 
 @app.post("/auth/basic/login")
